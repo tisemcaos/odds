bf3193125b00ed9694ceef5b0e6eef17400d714b
tisemcaos <dminformatica.2020@gmail.com>
2026-05-03 03:28:30 -0300

    Update and rename server.js to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// ⚠️ IMPORTANTE: Para GitHub Pages, crie este arquivo localmente
+// e adicione config.js no .gitignore se for repositório público!
+const CONFIG = {
+    API_KEY: 'sua_chave_api_aqui',
+    API_URL: 'https://v3.football.api-sports.io'
+};

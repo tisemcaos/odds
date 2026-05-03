af5e8418059b0f9fd3e8fb7c6c923341989c522d
tisemcaos <dminformatica.2020@gmail.com>
2026-05-03 03:35:45 -0300

    Update API_KEY in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ⚠️ IMPORTANTE: Para GitHub Pages, crie este arquivo localmente
 // e adicione config.js no .gitignore se for repositório público!
 const CONFIG = {
-    API_KEY: 'sua_chave_api_aqui',
+    API_KEY: 'd55c8b3a60ad39b9be34977140945d90',
     API_URL: 'https://v3.football.api-sports.io'
 };

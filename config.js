cbcff31177698e7e7a2bf1270535785132548ee1
tisemcaos <dminformatica.2020@gmail.com>
2026-05-04 11:44:42 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ⚠️ IMPORTANTE: Para GitHub Pages, crie este arquivo localmente
-// e adicione config.js no .gitignore se for repositório público!
-const CONFIG = {
-    API_KEY: '494ef147e7424296b7a2a7c6c1363185',
-    API_URL: 'https://v3.football.api-sports.io'
-};
